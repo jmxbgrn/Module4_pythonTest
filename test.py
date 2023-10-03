7f9c8d9bee06fccbad66472f1aace0f571d77ff0
GitHub username <GitHub-email-address>
2023-10-03 08:04:07 +0000

    Module 4

---
+++ test.py
@@ -0,0 +1,26 @@
+import math
+
+def test_sqrt():
+    num=25
+    assert math.sqrt(num) == 8
+
+def test_square():
+
+    num = 7
+    assert num*num == 48
+
+def test_check():
+    x = 100
+
+    assert xs>=100
+
+def test_multiply():
+    x = 4
+    y = 5
+
+    assert x*y == 25
+
+def test_fun():
+    x = 4
+
+    assert x+1 == 10
